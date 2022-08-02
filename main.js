90e1d23e0fd7322992e0ed18689b42f70c971a09
luiscorreagiraldo <luiscorrea250901@gmail.com>
2022-08-02 18:05:50 -0500

    I added the mobile and desktop menu to page, of course I used mediaquaries to have a Responsive Design

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const hamburger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
+
+
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
+hamburger.addEventListener("click", toggleMobileMenu);
+
+
+
+
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
+
+function hola () {
+  console.log('hola')
 }
