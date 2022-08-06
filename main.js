342ed61bfd26c81e47a9f13f4a8f5ab4f46bddbe
luiscorreagiraldo <luiscorrea250901@gmail.com>
2022-08-05 19:53:02 -0500

    JS no está correcto en remoto, probando otra vez

--- main.js
+++ main.js
@@ -6,50 +6,51 @@ const mobileMenu = document.querySelector(".mobile-menu");
 
 const carrito = document.querySelector(".navbar-shopping-cart");
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
-const productDetailContainer= document.querySelector("#productDetail")
+const productDetailContainer = document.querySelector("#productDetail");
+const productDetailCloseIcon = document.querySelector(".product-detail-close");
 
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 hamburger.addEventListener("click", toggleMobileMenu);
 carrito.addEventListener("click", details);
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside);
 
 
 
 
-
-
-
+//funciones
 
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
-  mobileMenu.classList.add("inactive")
-  shoppingCartContainer.classList.add("inactive")
+  mobileMenu.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
+
 }
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
-  desktopMenu.classList.add("inactive")
-  shoppingCartContainer.classList.add("inactive")
+  desktopMenu.classList.add("inactive");
+  shoppingCartContainer.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
 }
 
 function details() {
   shoppingCartContainer.classList.toggle("inactive");
-  desktopMenu.classList.add("inactive")
-  mobileMenu.classList.add("inactive")
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
 }
 
-function openProductDetailAside (){
-  productDetailContainer.classList.remove("inactive")
+function openProductDetailAside() {
+  productDetailContainer.classList.remove("inactive");
 }
-
-
-
-
-
-
-
+function closeProductDetailAside() {
+  console.log("holi,funciono");
+  productDetailContainer.classList.add("inactive");
+}
 
 const productList = [];
 productList.push({
@@ -73,17 +74,15 @@ productList.push({
   image: "./Smoking.jpg",
 });
 
-
 function renderProducts(arr) {
   for (product of arr) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
 
-
     // PRODUCT= {name,price, image}=> product.image
     const img = document.createElement("img");
     img.setAttribute("src", product.image);
-    img.addEventListener("click", openProductDetailAside)
+    img.addEventListener("click", openProductDetailAside);
 
     const productInfo = document.createElement("div");
     productInfo.classList.add("product-info");
@@ -101,7 +100,7 @@ function renderProducts(arr) {
     const productInfoFigure = document.createElement("figure");
     const productImg = document.createElement("img");
     productImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
-    productImg.classList.add("product-cardImg")
+    productImg.classList.add("product-cardImg");
 
     productInfoFigure.appendChild(productImg);
 
@@ -114,7 +113,47 @@ function renderProducts(arr) {
     cardsContainer.appendChild(productCard);
   }
 }
-renderProducts(productList)
+renderProducts(productList);
 
 
 
+//experimento
+// const productDetailedImage = document.querySelector("#product-detailed-image");
+// productDetailedImage.setAttribute("src", product.image);
+//experimento
+
+// const productDetailIndividual = document.querySelector('.-detail-indiviproductdual');
+
+// function openProductDetail(evento)
+// {
+//     productDetailIndividual.classList.remove('hidden');
+//     productDetail.classList.add('hidden');
+//     const lista = productList;
+//     const elemento = lista.find(producto => producto.image == evento.explicitOriginalTarget.src);
+//     console.log(elemento);
+
+//     const mostrarElemento = `
+//     <div class="product-detail-individual-close">
+//         <img src="./icons/icon_close.png" alt="close" class="close-product-detail">
+//     </div>
+//      <img src=${elemento.image} alt='${elemento.name}' title='${elemento.name}'>
+//     <div class="product-info">
+//         <p>$${elemento.price}</p>
+//         <p>${elemento.name}</p>
+//         <p>${elemento.description}.</p>
+//         <button class="primary-button add-to-cart-button">
+//             <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+//             Add to cart
+//          </button>
+//     </div>`;
+//     const prueba = document.querySelector('.product-detail-individual');
+//     prueba.innerHTML = mostrarElemento;
+
+//     const closeProductDetail = document.querySelector('.product-detail-individual-close');
+//     closeProductDetail.addEventListener('click', closedProductDetail);
+//     function closedProductDetail()
+//     {
+//         productDetailIndividual.classList.add('hidden');
+//     }
+// }
+//https://github.com/Mora-Antonio/NewJavaScript
